8f5a5ec4f78136560585e2926faff03fae1ab8fe
Mandrako <marcus@olkerud.com>
2015-10-01 14:18:25 +0200

    Enabled transparent blending

--- MainSolution/MainProject/Game.cs
+++ MainSolution/MainProject/Game.cs
@@ -24,7 +24,8 @@ namespace MainProject
 
         void window_Load(object sender, EventArgs e)
         {
-            
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
         }
 
         void window_UpdateFrame(object sender, FrameEventArgs e)
@@ -36,6 +37,30 @@ namespace MainProject
         {
             GL.ClearColor(Color.CornflowerBlue);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.Red);
+            GL.Vertex2(0, 0);
+
+            GL.Color3(Color.Green);
+            GL.Vertex2(1, 0);
+
+            GL.Color3(Color.Blue);
+            GL.Vertex2(0, 1);
+
+            // ---
+
+            GL.Color4(1f, 0f, 0f, 0.5f);
+            GL.Vertex2(-0.25f, 1);
+
+            GL.Color4(0f, 1f, 0f, 0.5f);
+            GL.Vertex2(1, -0.25f);
+
+            GL.Color4(0f, 0f, 1f, 0.5f);
+            GL.Vertex2(-0.25f, -0.25f);
+
+            GL.End();
 
             window.SwapBuffers();
         }
